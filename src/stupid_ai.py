e79f8bb648f9c9b944a530b13b766c8ffaaa6c8a
Paul Veltheim <27195593+ankkalampi@users.noreply.github.com>
2025-10-07 06:03:24 +0300

    fiddled with bitboards

--- src/stupid_ai.py
+++ src/stupid_ai.py
@@ -1,8 +1,43 @@
 import random
-import chess
 import time
 
+class ChessBoard:
+    def __init__(self):
+        # init bitboards for each piece type
+        self.white_pawns    = 0x000000000000FF00
+        self.white_knights  = 0x0000000000000042
+        self.white_bishops  = 0x0000000000000024
+        self.white_rooks    = 0x0000000000000081
+        self.white_queens   = 0x0000000000000008
+        self.white_king     = 0x0000000000000010
 
+        self.black_pawns    = 0x00FF000000000000
+        self.black_knights  = 0x4200000000000000
+        self.black_bishops  = 0x2400000000000000
+        self.black_rooks    = 0x8100000000000000
+        self.black_queens   = 0x0800000000000000
+        self.black_king     = 0x1000000000000000
+
+        self.white_to_move = True
+
+    def generate_pawn_moves(self):
+        moves = []
+
+        # calculate empty squares (complement of all squares with pieces)
+        empty_squares = ~(self.white_pawns | self.white_knights | self.white_bishops |
+                            self.white_rooks | self.white_queens | self.white_king |
+                            self.black_pawns | self.black_knights | self.black_bishops |
+                            self.black_rooks | self.black_queens | self.black_king)
+        
+        # calculate legal moves for who is to move
+        if (self.white_to_move):
+            # calculate single square pawn moves (shifting bits per 8 moves up one rank)
+            pawn_moves = (self.white_pawns << 8) & empty_squares
+
+            # calculate double square moves 
+            # (moving piece must be in starting position, destination square must be empty,
+            # and the square in between has to be empty as well)
+            double_moves = ((self.white_pawns & 0x000000000000FF00) << 16) & empty_squares & (empty_squares << 8)
 
 
 
@@ -18,9 +53,14 @@ def make_move(board: chess.Board):
 
     return choice
 
+
+
+
+
+
 def main():
 
-    board = chess.Board()
+    #board = chess.Board()
 
     while True:
         opponent_move = input()
