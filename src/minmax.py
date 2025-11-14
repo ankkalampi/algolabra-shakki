f793fc320d60c26cbba7b8d672253dd833e7dd5d
Paul Veltheim <paul.veltheim@gmail.com>
2025-11-15 01:35:43 +0200

    started minmax

---
+++ src/minmax.py
@@ -0,0 +1,13 @@
+from src.situation import generate_situation, get_moves
+from src.heuristic import get_highest_value_move, get_lowest_value_move, evaluate_situation
+
+def get_move_using_minmax(situation, depth):
+    pass
+
+
+
+
+    
+        
+
+
